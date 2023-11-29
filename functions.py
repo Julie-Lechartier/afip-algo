9547160b22caf934d0e8734e21d914e253e66a1d
Julie_LECHARTIER <julie.lechartier@gmail.com>
2023-11-29 08:39:59 +0100

    niveau 1 étape 1 et 2

--- functions.py
+++ functions.py
@@ -1,2 +1,21 @@
-
 import os
+
+#1. Fonction `charger_donnees(fichier: str) -> list`
+fichier = open("FIFA_World_cup/FIFA-2022.txt", "r")
+print(fichier.read())
+fichier.close()
+
+#2. Fonction `supprimer_entete(fichier: str) -> list`
+with open ("FIFA_World_cup/FIFA-2022.txt", "r+") as fichier:
+    ligne1 = fichier.readlines()
+    fichier.seek(0)
+    fichier.truncate()
+    fichier.writelines(ligne1[1:])
+fichier.close()
+
+#3. Fonction `afficher_nom_trois_lettres(str) -> str`
+#[:2].upper() placement et masjucule
+# couper = slice(3)
+#split
+with open ("FIFA_World_cup/FIFA-2022.txt", "r+") as fichier:
+    print(fichier.split(','))
