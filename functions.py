21372c195ec64245b0ac927eb5edade60c0d7559
Julie_LECHARTIER <julie.lechartier@gmail.com>
2023-12-06 09:19:43 +0100

    changement fonction 1 et 2, debut de la fonction 3

--- functions.py
+++ functions.py
@@ -1,21 +1,38 @@
 import os
 
+
+
+FifaText = "FIFA_World_cup/FIFA-2022.txt";
+file = open(FifaText, "r+");
+
 #1. Fonction `charger_donnees(fichier: str) -> list`
-fichier = open("FIFA_World_cup/FIFA-2022.txt", "r")
-print(fichier.read())
-fichier.close()
+print(file.read());
+file.close()
 
 #2. Fonction `supprimer_entete(fichier: str) -> list`
-with open ("FIFA_World_cup/FIFA-2022.txt", "r+") as fichier:
-    ligne1 = fichier.readlines()
-    fichier.seek(0)
-    fichier.truncate()
-    fichier.writelines(ligne1[1:])
-fichier.close()
+
+with open(FifaText, "r+") as supp:
+    lignes = supp.readlines();
+    supp.seek(0);
+    supp.truncate();
+    supp.writelines(lignes[1:]);
+    supp.close()
+#comment afficher le résultat ?
+
 
 #3. Fonction `afficher_nom_trois_lettres(str) -> str`
+with open(FifaText, "r+") as file:
+    lignecol = file.readlines();
+    colonnes = [lignecol.strip().split(",") for ligne in lignecol];
+    for ligne in colonnes:
+        print('\t'.join(ligne))
+file.close()
+
 #[:2].upper() placement et masjucule
 # couper = slice(3)
 #split
-with open ("FIFA_World_cup/FIFA-2022.txt", "r+") as fichier:
-    print(fichier.split(','))
+#with open ("FIFA_World_cup/FIFA-2022.txt", "r+") as fichier:
+#    print(fichier.split(','))
+
+#fichier = pd.read_csv("FIFA_World_cup/FIFA-2022.txt", sep=" ", header=None)
+#print(fichier)
