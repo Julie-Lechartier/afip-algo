1dc08aafecd214f92dbb6514810a6e8cbc9c73a5
Julie_LECHARTIER <julie.lechartier@gmail.com>
2023-12-07 10:19:31 +0100

    étape 1 et 2 et appel des fonctions dans main

--- functions.py
+++ functions.py
@@ -1,33 +1,49 @@
 import os
 
-
-
-FifaText = "FIFA_World_cup/FIFA-2022.txt";
-file = open(FifaText, "r+");
+fileName = "FIFA_World_cup/FIFA-2022.txt";
 
 #1. Fonction `charger_donnees(fichier: str) -> list`
-print(file.read());
-file.close()
+def Charger_donner (data):
+    data = open(fileName, "r+")
+   
 
 #2. Fonction `supprimer_entete(fichier: str) -> list`
+def supprimer_entete(data):
+    lignes = data.readlines()
+    f = open("text.txt", 'a+')
+    f.writelines(lignes[1:])
+    f.close()
 
-with open(FifaText, "r+") as supp:
-    lignes = supp.readlines();
-    supp.seek(0);
-    supp.truncate();
-    supp.writelines(lignes[1:]);
-    supp.close()
+
+#with open(Texte, "r+") as supp:
+#    lignes = supp.readlines();
+#    supp.seek(0);
+#    supp.truncate();
+#    supp.writelines(lignes[1:]);
 #comment afficher le résultat ?
 
 
 #3. Fonction `afficher_nom_trois_lettres(str) -> str`
-with open(FifaText, "r+") as file:
-    lignecol = file.readlines();
-    colonnes = [lignecol.strip().split(",") for ligne in lignecol];
-    for ligne in colonnes:
-        print('\t'.join(ligne))
-file.close()
+def affichier_nom_trois_lettres(supprimer_entete):
+    lignecol = supprimer_entete.readlines();
+    for ligne in lignecol:
+        sep = ","
+        acc = 0
+        sequence = ''
+        if ligne == sep:
+            acc = acc + 1
+        if acc > 0 and acc <= 1 and ligne != sep :
+            sequence += ligne;
+    team = sequence[0]+sequence[1]+sequence[2]
+    print(team)
 
+
+#    print(ligne)     
+#file.close()
+    
+#char name[50]:
+#    name[0] = "F" création d'un index
+#fonction char[50] on déclare le nombre de place à réserver
 #[:2].upper() placement et masjucule
 # couper = slice(3)
 #split
--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 from functions import *
 
 # Nom de fichier à charger
-fileName = "";
+fileName = "FIFA_World_cup/FIFA-2022.txt";
 
 # Charge un fichier en lecture
-file = open(fileName, "r")
+data = open(fileName, "r+")
 
 # Charge les données à partir d'un fichier
-data = load_data(fileName);
+print(Charger_donner(data))
 
+
+#suppression de l'en-tête
+print(supprimer_entete(data))
+
+
+#affichage de trois lettres
+print(affichier_nom_trois_lettres(supprimer_entete))
