da36da8b48c779ab2fdd6e4045dc0c2e935b8a8e
Julie_LECHARTIER <julie.lechartier@gmail.com>
2023-12-19 21:48:46 +0100

    fonction trois lettres

--- functions.py
+++ functions.py
@@ -9,32 +9,40 @@ def Charger_donner (fileName):
         return file.readlines()
    
 
-#2. Fonction `supprimer_entete(fichier: str) -> list`
-def supprimer_entete(fileName):
-    with open(fileName, 'r+') as donne:
-        lignes = donne.readlines()
-        donne.seek(0);
-        donne.truncate();
-        donne.writelines(lignes[1:]);
-
-#4. Fonction `afficher_nom_trois_lettres(str) -> str`
-def affichier_nom_trois_lettres(data, fichier2):
-    with open(fichier2, 'w') as resultat:
-        for ligne in data:
-            colonnes = ligne.split(',')
-            if len(colonnes) >=2:
-                team = colonnes[1][:3]
-                resultat.write(team +'\n')
-            else:
-                print("Impossible de faire le changement")
-
-
-#5. Fonction `enregistrer_fichier("donnees, fichier.ext") -> fichier.ext`
+#2. Fonction `enregistrer_fichier("donnees, fichier.ext") -> fichier.ext`
 def enregistrer_fichier(data, fichier2):
     with open(fichier2, 'w') as fichier_secondaire:
         fichier_secondaire.writelines(data)
     print("Texte FIFA mis a jour.")
     
+
+
+#3. Fonction `supprimer_entete(fichier: str) -> list`
+def supprimer_entete(data, fichier2):
+    with open(fichier2, 'r+') as donne:
+        lignes = donne.readlines()
+        donne.seek(0);
+        donne.truncate();
+        donne.writelines(lignes[1:]);
+        
+#3. fonction transformer le fichier2 sous forme de colonne
+def transformation_colonne(data, fichier2):
+    with open(fichier2, 'w') as file:
+        for line in data:
+            colonnes = line.strip().split(',')
+            file.write(','.join(colonnes) + '\n')   
+
+#4. Fonction `afficher_nom_trois_lettres(str) -> str`
+def affichier_nom_trois_lettres(data, fichier2):
+    with open(fichier2, 'w') as resultat:
+        for ligne in data:
+            colonnes =ligne.strip().split(',')
+            if len(colonnes) >=2:
+                colonnes[1] = colonnes[1][:3]
+                resultat.write(','.join(colonnes) + '\n')
+            else:
+                print("Impossible de faire le changement")
+   
 
 #6. mélanger les lignes d'un fichier
 def melanger_lignes(fichier2):
@@ -43,5 +51,6 @@ def melanger_lignes(fichier2):
         random.shuffle(lines)
     with open(fichier2, 'w') as file:
         file.writelines(lines)
+
 
 
--- main.py
+++ main.py
@@ -7,12 +7,17 @@ fileName = "FIFA_World_cup/FIFA-2022.txt";
 data = Charger_donner(fileName);
 print(data)
 
-#suppression de l'en-tête
-print(supprimer_entete(fileName))
-
 #nouveau fichier copié
 fichier2 = "text.txt"
-enregistrer_fichier(data, "text.txt")
+enregistrer_fichier(data, fichier2)
+
+#transformation des lignes en colonnes
+transformation_colonne(data, fichier2)
+
+#suppression de l'en-tête
+supprimer_entete(data, fichier2)
+
+
 
 #affichage de trois lettres
 affichier_nom_trois_lettres(data, fichier2)
