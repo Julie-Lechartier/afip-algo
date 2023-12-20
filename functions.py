f2c7c0592846e34abd8e159c654499ef1f81b1f5
Julie_LECHARTIER <julie.lechartier@gmail.com>
2023-12-20 08:57:54 +0100

    ajout calcul addition

--- functions.py
+++ functions.py
@@ -7,30 +7,31 @@ fileName = "FIFA_World_cup/FIFA-2022.txt";
 def Charger_donner (fileName):
     with open(fileName, "r+") as file:
         return file.readlines()
-   
+    
+#3. Fonction `supprimer_entete(fichier: str) -> list`
+def supprimer_entete(fileName):
+    with open(fileName, 'r+') as donne:
+        lignes = donne.readlines()
+        donne.seek(0);
+        donne.truncate();
+        donne.writelines(lignes[1:]);
+        
+
 
 #2. Fonction `enregistrer_fichier("donnees, fichier.ext") -> fichier.ext`
 def enregistrer_fichier(data, fichier2):
     with open(fichier2, 'w') as fichier_secondaire:
         fichier_secondaire.writelines(data)
     print("Texte FIFA mis a jour.")
-    
 
-
-#3. Fonction `supprimer_entete(fichier: str) -> list`
-def supprimer_entete(data, fichier2):
-    with open(fichier2, 'r+') as donne:
-        lignes = donne.readlines()
-        donne.seek(0);
-        donne.truncate();
-        donne.writelines(lignes[1:]);
         
 #3. fonction transformer le fichier2 sous forme de colonne
 def transformation_colonne(data, fichier2):
     with open(fichier2, 'w') as file:
-        for line in data:
-            colonnes = line.strip().split(',')
+        for ligne in data:
+            colonnes = ligne.strip().split(',')
             file.write(','.join(colonnes) + '\n')   
+
 
 #4. Fonction `afficher_nom_trois_lettres(str) -> str`
 def affichier_nom_trois_lettres(data, fichier2):
@@ -47,10 +48,24 @@ def affichier_nom_trois_lettres(data, fichier2):
 #6. mélanger les lignes d'un fichier
 def melanger_lignes(fichier2):
     with open(fichier2, 'r') as file:
-        lines = file.readlines()
-        random.shuffle(lines)
+        lignes = file.readlines()
+        random.shuffle(lignes)
     with open(fichier2, 'w') as file:
-        file.writelines(lines)
+        file.writelines(lignes)
+        
+#7. Fonction `calculer_points(fichier: str) -> dict`
+def calculer_points(fichier2):
+    with open(fichier2, 'r') as addition:
+        lignes = addition.readlines()
+        for ligne in lignes:
+            colonnes =ligne.strip().split(',')
+            if len(colonnes) >= 6:
+                colonne5 = int(colonnes[4])
+                colonne6 = int(colonnes[5])
+               
+                print( colonne5 + colonne6)
+            else:
+                print("Il n'y a pas de match dans cet équipe")
 
 
 
--- main.py
+++ main.py
@@ -7,15 +7,21 @@ fileName = "FIFA_World_cup/FIFA-2022.txt";
 data = Charger_donner(fileName);
 print(data)
 
+#suppression de l'en-tête
+supprimer_entete(fileName)
+
 #nouveau fichier copié
 fichier2 = "text.txt"
 enregistrer_fichier(data, fichier2)
 
+
+
+
+
 #transformation des lignes en colonnes
 transformation_colonne(data, fichier2)
 
-#suppression de l'en-tête
-supprimer_entete(data, fichier2)
+
 
 
 
@@ -25,3 +31,6 @@ affichier_nom_trois_lettres(data, fichier2)
 
 #melanger les lignes
 print(melanger_lignes(fichier2))
+
+#calculer les point 
+calculer_points(fichier2)
